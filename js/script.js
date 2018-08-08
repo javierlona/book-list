15df7fb89099d68ae97a70dd3698c8b0620e95e4
Javier Lona <javierlona1@gmail.com>
2018-08-08 07:40:17 -0500

    identified alert issue

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-FORM = document.querySelector("#book-form");
+const FORM = document.querySelector("#book-form");
+const CONTAINER = document.querySelector(".row");
 
 // Book constructor
 function Book(title, author, isbn) {
@@ -28,14 +29,31 @@ UI.prototype.add_book_to_list = function(book){
 }
 
 // Show Alert
-UI.prototype.showAlert = function(message, theClassName) {
+// UI.prototype.show_alert = function(message, theClassName) {
+//   // Create div
+//   const DIV = document.createElement("div");
+//   // Add classes
+//   DIV.className = `alert ${theClassName}`;
+//   // Add text
+//   DIV.appendChild(document.createTextNode(message));
+//   console.log(DIV);
+//   // Insert alert
+//   CONTAINER.insertBefore(DIV, FORM);
+
+//   // Timeout after 3 seconds
+//   // setTimeout(function() {
+//   //   document.querySelector('.alert').remove();}, 3000);
+// }
+
   // Create div
   const DIV = document.createElement("div");
   // Add classes
-  DIV.className = `alert ${className}`;
+  DIV.className = "alert error";
   // Add text
-  DIV.append(message);
-}
+  DIV.append("message in a bottle");
+  console.log(DIV);
+  // Insert alert
+  CONTAINER.insertBefore(DIV, FORM);
 
 // Event Listeners
 FORM.addEventListener('submit', function(e){
@@ -52,6 +70,7 @@ FORM.addEventListener('submit', function(e){
   // Validate
   if(title === '' || author === '' || isbn === '') {
     alert('Failed');
+    ui.show_alert('Please fill in all fields', 'error');
   } else {
     // Add book to list
     ui.add_book_to_list(book);
