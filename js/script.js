bbfc43e4fb2250a95591fcd25ba34277cc954d91
Javier Lona <javierlona1@gmail.com>
2018-08-07 20:28:44 -0500

    connect js script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+// UI constructor
+function UI() {
+
+}
+// Event Listeners
+document.getElementById("book-form").addEventListener('submit', function(e){
+  console.log("Test");
+  e.preventDefault();
+});
